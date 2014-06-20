88bcf2e0fe0d2b98174efed7f0b809139cd0e79c
rmcdaniel <richard.mcdaniel@uah.edu>
2014-06-20 10:02:04 -0500

    Formatting.

--- api/application/controllers/account.php
+++ api/application/controllers/account.php
@@ -2,31 +2,31 @@
 
 class Account extends CI_Controller {
 
-    private function authenticate()
-    {
-        $this->form_validation->set_rules('token', 'token', 'required');
+	private function authenticate()
+	{
+		$this->form_validation->set_rules('token', 'token', 'required');
 		$validated = $this->form_validation->run();
 		if ($validated)
 		{
 			$token = $this->input->post('token');
-            return JWT::decode($token, $this->config->item('jwt_key'));
+			return JWT::decode($token, $this->config->item('jwt_key'));
 		}
 		else
 		{
-            $output['status'] = false;
-            $output['errors'] = validation_errors();
-            $this->load->view('json', array('output' => $output));
-            return false;
+			$output['status'] = false;
+			$output['errors'] = validation_errors();
+			$this->load->view('json', array('output' => $output));
+			return false;
 		}
-    }
+	}
 
 	public function login()
 	{
-	    $output = array();
-        $output['status'] = false;
+		$output = array();
+		$output['status'] = false;
 		$this->form_validation->set_rules('email', 'email', 'required|valid_email|max_length[256]');
 		$this->form_validation->set_rules('password', 'password', 'required|min_length[8]|max_length[256]');
-        $this->form_validation->set_error_delimiters('', '');
+		$this->form_validation->set_error_delimiters('', '');
 		$validated = $this->form_validation->run();
 		if ($validated)
 		{
@@ -34,60 +34,60 @@ class Account extends CI_Controller {
 			$password = $this->input->post('password');
 			$id = $this->Accounts->login($email, $password);
 			if ($id != false) {
-        	    $token = array();
-        	    $token['id'] = $id;
-                $output['status'] = true;
-                $output['email'] = $email;
-                $output['token'] = JWT::encode($token, $this->config->item('jwt_key'));
+				$token = array();
+				$token['id'] = $id;
+				$output['status'] = true;
+				$output['email'] = $email;
+				$output['token'] = JWT::encode($token, $this->config->item('jwt_key'));
 			}
-    		else
-    		{
-                $output['errors'] = 'The email/password combination entered is invalid.';
-    		}
+			else
+			{
+				$output['errors'] = 'The email/password combination entered is invalid.';
+			}
 		}
 		else
 		{
-            $output['errors'] = validation_errors();
+			$output['errors'] = validation_errors();
 		}
-        $this->load->view('json', array('output' => $output));
+		$this->load->view('json', array('output' => $output));
 	}
 	
 	public function register()
 	{
-	    $output = array();
-        $output['status'] = false;
+		$output = array();
+		$output['status'] = false;
 		$this->form_validation->set_rules('email', 'email', 'required|valid_email|is_unique[accounts.email]|max_length[256]');
 		$this->form_validation->set_rules('password', 'password', 'required|min_length[8]|max_length[256]');
-        $this->form_validation->set_error_delimiters('', '');
+		$this->form_validation->set_error_delimiters('', '');
 		$validated = $this->form_validation->run();
 		if ($validated)
 		{
 			$email = $this->input->post('email');
 			$password = $this->input->post('password');
 			$this->Accounts->register($email, $password);
-            $output['status'] = true;
+			$output['status'] = true;
 		}
 		else
 		{
-            $output['errors'] = validation_errors();
+			$output['errors'] = validation_errors();
 		}
-        $this->load->view('json', array('output' => $output));
+		$this->load->view('json', array('output' => $output));
 	}
 	
 	public function information() {
-        $output = array();
-        $output['status'] = false;
-	    $token = $this->authenticate();
-        if ($token !== false)
-        {
-            $output['status'] = true;
-            $output['message'] = 'Hello!';
-        }
-        else
-        {
-            $output['errors'] = 'You must login first.';
-        }
-        $this->load->view('json', array('output' => $output));
+		$output = array();
+		$output['status'] = false;
+		$token = $this->authenticate();
+		if ($token !== false)
+		{
+			$output['status'] = true;
+			$output['message'] = 'Hello!';
+		}
+		else
+		{
+			$output['errors'] = 'You must login first.';
+		}
+		$this->load->view('json', array('output' => $output));
 	}
 	
 }
