29bef1e17455522342f1c719cd2f9ea628623324
Richard McDaniel <richard.mcdaniel@uah.edu>
2015-05-14 23:00:27 -0400

    login test

--- tests/run.php
+++ tests/run.php
@@ -9,13 +9,6 @@ class Run extends Sauce\Sausage\WebDriverTestCase
 
     public static $browsers = array(
         array(
-            'browserName' => 'firefox',
-            'desiredCapabilities' => array(
-                'version' => '15',
-                'platform' => 'Windows 2012',
-            )
-        ),
-        array(
             'browserName' => 'chrome',
             'desiredCapabilities' => array(
                 'platform' => 'Linux'
@@ -28,37 +21,22 @@ class Run extends Sauce\Sausage\WebDriverTestCase
         $this->assertContains("AngularJS CodeIgniter Seed", $this->title());
     }
 
-    // public function testLink()
-    // {
-    //     $link = $this->byId('i am a link');
-    //     $link->click();
-    //     $this->assertContains("I am another page title", $this->title());
-    // }
 
-    // public function testTextbox()
-    // {
-    //     $test_text = "This is some text";
-    //     $textbox = $this->byId('i_am_a_textbox');
-    //     $textbox->click();
-    //     $textbox->clear();
-    //     $this->keys($test_text);
-    //     $this->assertEquals($textbox->value(), $test_text);
-    // }
+    public function testUrl()
+    {
+        $this->assertContains("http://localhost/#/home", $this->url());
+    }
 
-    // public function testSubmitComments()
-    // {
-    //     $comment = "This is a very insightful comment.";
-    //     $this->byId('comments')->value($comment);
-    //     $this->byId('submit')->submit();
-    //     $driver = $this;
-
-    //     $comment_test = function() use ($comment, $driver) {
-    //         $text = $driver->byId('your_comments')->text();
-    //         return $text == "Your comments: $comment";
-    //     };
-
-    //     $this->spinAssert("Comment never showed up!", $comment_test);
-
-    // }
+    public function testLogin()
+    {
+        $this->url('http://localhost/#/login');
+        $this->assertTextPresent("Login");
+        $this->assertTextNotPresent("Logout");
+        $this->byName('email')->value('foo@bar.com');
+        $this->byName('password')->value('password123');
+        $this->byName('login')->submit();
+        $this->assertTextPresent("Logout");
+        $this->assertTextNotPresent("Login");
+    }
 
 }
