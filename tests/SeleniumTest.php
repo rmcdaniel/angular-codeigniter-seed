8255e5b0b0c5b1643e89c2ba604622866cae8567
Richard McDaniel <richard.mcdaniel@uah.edu>
2015-05-15 23:46:26 -0400

    fix

---
+++ tests/SeleniumTest.php
@@ -0,0 +1,40 @@
+<?php
+
+// require_once 'vendor/autoload.php';
+
+// class Run extends Sauce\Sausage\WebDriverTestCase
+// {
+
+//     protected $start_url = 'http://localhost';
+
+//     public static $browsers = array(
+//         array(
+//             'browserName' => 'chrome',
+//             'desiredCapabilities' => array(
+//                 'platform' => 'Linux'
+//           )
+//         )
+//     );
+
+//     public function testTitle()
+//     {
+//         $this->assertContains("AngularJS CodeIgniter Seed", $this->title());
+//     }
+
+
+//     public function testUrl()
+//     {
+//         $this->assertContains("http://localhost/#/home", $this->url());
+//     }
+
+//     public function testLogin()
+//     {
+//         $this->url('http://localhost/#/login');
+//         $this->assertTextNotPresent("Logout");
+//         $this->byName('email')->value('foo@bar.com');
+//         $this->byName('password')->value('password123');
+//         $this->byName('login')->submit();
+//         $this->assertTextPresent("Logout");
+//     }
+
+// }
