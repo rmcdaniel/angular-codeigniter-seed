56d64263d1c5ffc8ef535353534d504b5dabe57b
Richard McDaniel <richard.mcdaniel@uah.edu>
2015-05-15 23:58:10 -0400

    fix

--- tests/SeleniumTest.php
+++ tests/SeleniumTest.php
@@ -1,40 +1,40 @@
 <?php
 
-// require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-// class Run extends Sauce\Sausage\WebDriverTestCase
-// {
+class Run extends Sauce\Sausage\WebDriverTestCase
+{
 
-//     protected $start_url = 'http://localhost';
+    protected $start_url = 'http://localhost';
 
-//     public static $browsers = array(
-//         array(
-//             'browserName' => 'chrome',
-//             'desiredCapabilities' => array(
-//                 'platform' => 'Linux'
-//           )
-//         )
-//     );
+    public static $browsers = array(
+        array(
+            'browserName' => 'chrome',
+            'desiredCapabilities' => array(
+                'platform' => 'Linux'
+          )
+        )
+    );
 
-//     public function testTitle()
-//     {
-//         $this->assertContains("AngularJS CodeIgniter Seed", $this->title());
-//     }
+    public function testTitle()
+    {
+        $this->assertContains("AngularJS CodeIgniter Seed", $this->title());
+    }
 
 
-//     public function testUrl()
-//     {
-//         $this->assertContains("http://localhost/#/home", $this->url());
-//     }
+    public function testUrl()
+    {
+        $this->assertContains("http://localhost/#/home", $this->url());
+    }
 
-//     public function testLogin()
-//     {
-//         $this->url('http://localhost/#/login');
-//         $this->assertTextNotPresent("Logout");
-//         $this->byName('email')->value('foo@bar.com');
-//         $this->byName('password')->value('password123');
-//         $this->byName('login')->submit();
-//         $this->assertTextPresent("Logout");
-//     }
+    public function testLogin()
+    {
+        $this->url('http://localhost/#/login');
+        $this->assertTextNotPresent("Logout");
+        $this->byName('email')->value('foo@bar.com');
+        $this->byName('password')->value('password123');
+        $this->byName('login')->submit();
+        $this->assertTextPresent("Logout");
+    }
 
-// }
+}
--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -1,4 +1,5 @@
 <?php
+
 class UnitTest extends PHPUnit_Framework_TestCase
 {
     private $CI;
