673b7ac44704816d34e7d06f415f69f6fcaa236f
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-05-29 02:15:54 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- api/application/models/roles.php
+++ api/application/models/roles.php
@@ -2,41 +2,41 @@
 
 class Roles extends CI_Model {
 
-    public function table($params)
-    {
+	public function table($params)
+	{
 		$acl = new ACL();
-        $roles = $acl->roles();
+		$roles = $acl->roles();
 
-        $data = array();
-        $data['total'] = count($roles);
-        $data['roles'] = $roles;
-        return $data;
-    }
+		$data = array();
+		$data['total'] = count($roles);
+		$data['roles'] = $roles;
+		return $data;
+	}
     
-    public function create($role)
-    {
+	public function create($role)
+	{
 		$acl = new ACL();
 		$acl->addRole($role);
-    }    
+	}    
 
-    public function read($role, $resource)
-    {
+	public function read($role, $resource)
+	{
 		$acl = new ACL();
 		return $acl->rolePermissions($role, $resource);
-    }
+	}
 
-    public function update($role, $resource, $permissions)
-    {
+	public function update($role, $resource, $permissions)
+	{
 		$acl = new ACL();
 		$acl->removePermissions($role, $resource, $acl->rolePermissions($role, $resource));
 		$acl->addPermissions($role, $resource, $permissions);
-    }
+	}
     
-    public function delete($role)
-    {
+	public function delete($role)
+	{
 		$acl = new ACL();
 		$acl->removeRole($role);
-    }    
+	}    
     
 }
 
--- api/application/models/users.php
+++ api/application/models/users.php
@@ -29,97 +29,97 @@ class Users extends CI_Model {
 		return $this->db->insert_id();
 	}
 
-    public function table($params)
-    {
-        $limit = intval($params->count);
-        $offset = intval(($params->page - 1) * $params->count);
-        $sorting = get_object_vars($params->sorting);
-        $direction = reset($sorting);
-        $key = key($sorting);
-        if ($limit > 100) return;
-        if ($limit < 0) return;
-        if ($offset < 0) return;
-        if (!in_array($direction, array('asc', 'desc'))) return;
-        if (!in_array($key, array('id', 'email'))) return;
-        $this->db->select('*');
-        $this->db->from('users');
-        $this->db->order_by($key, $direction);
-        $this->db->limit($limit, $offset);
-        $query = $this->db->get();
-        if ($query->num_rows() >= 1)
-        {
-            $results = $query->result();
-        }
-        $users = array();
-        foreach ($results as $result)
-        {
-            $user = new stdClass();
-            $user->id = $result->id;
-            $user->email = $result->email;
-            $users[] = $user;
-        }
-        $total = $this->db->count_all('users');
+	public function table($params)
+	{
+		$limit = intval($params->count);
+		$offset = intval(($params->page - 1) * $params->count);
+		$sorting = get_object_vars($params->sorting);
+		$direction = reset($sorting);
+		$key = key($sorting);
+		if ($limit > 100) return;
+		if ($limit < 0) return;
+		if ($offset < 0) return;
+		if (!in_array($direction, array('asc', 'desc'))) return;
+		if (!in_array($key, array('id', 'email'))) return;
+		$this->db->select('*');
+		$this->db->from('users');
+		$this->db->order_by($key, $direction);
+		$this->db->limit($limit, $offset);
+		$query = $this->db->get();
+		if ($query->num_rows() >= 1)
+		{
+			$results = $query->result();
+		}
+		$users = array();
+		foreach ($results as $result)
+		{
+			$user = new stdClass();
+			$user->id = $result->id;
+			$user->email = $result->email;
+			$users[] = $user;
+		}
+		$total = $this->db->count_all('users');
         
-        $data = array();
-        $data['total'] = $total;
-        $data['users'] = $users;
-        return $data;
-    }	
+		$data = array();
+		$data['total'] = $total;
+		$data['users'] = $users;
+		return $data;
+	}	
 
-    public function read($id)
-    {
-        $user = new stdClass();
+	public function read($id)
+	{
+		$user = new stdClass();
 
-        $this->db->select('*');
-        $this->db->from('users');
+		$this->db->select('*');
+		$this->db->from('users');
 		$this->db->where('id', $id);
-        $this->db->limit(1);
+		$this->db->limit(1);
 
-        $query = $this->db->get();
-        if ($query->num_rows() == 1)
-        {
-            $result = $query->result();
-            $user->id = $result[0]->id;
-            $user->email = $result[0]->email;
-    		$acl = new ACL();
-            $user->roles = $acl->userRoles($user->id);
-        }
+		$query = $this->db->get();
+		if ($query->num_rows() == 1)
+		{
+			$result = $query->result();
+			$user->id = $result[0]->id;
+			$user->email = $result[0]->email;
+			$acl = new ACL();
+			$user->roles = $acl->userRoles($user->id);
+		}
 
-        return $user;
-    }
+		return $user;
+	}
 
-    public function update($user)
-    {
-        $this->db->select('*');
-        $this->db->from('users');
+	public function update($user)
+	{
+		$this->db->select('*');
+		$this->db->from('users');
 		$this->db->where('id', $user->id);
-        $this->db->limit(1);
+		$this->db->limit(1);
 
-        $query = $this->db->get();
-        if ($query->num_rows() == 1)
-        {
-    		$acl = new ACL();
-            $roles = $acl->userRoles($user->id);
-            if ($user->roles != $roles)
-            {
-                $acl->removeUserRoles($user->id, $roles);
-                $acl->addUserRoles($user->id, $user->roles);
-            }
-            $this->db->where('id', $user->id);
-            $this->db->update('users', $user);             
+		$query = $this->db->get();
+		if ($query->num_rows() == 1)
+		{
+			$acl = new ACL();
+			$roles = $acl->userRoles($user->id);
+			if ($user->roles != $roles)
+			{
+				$acl->removeUserRoles($user->id, $roles);
+				$acl->addUserRoles($user->id, $user->roles);
+			}
+			$this->db->where('id', $user->id);
+			$this->db->update('users', $user);             
             
-            $user = $this->read($user->id);
-        }
+			$user = $this->read($user->id);
+		}
 
-        return $user;
-    }	
+		return $user;
+	}	
 
-    public function delete($id)
-    {
-        $this->db->where('id', $id);
-        $this->db->delete('users');
-        $this->db->limit(1);
-    }
+	public function delete($id)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete('users');
+		$this->db->limit(1);
+	}
     
 }
 
